0c05c06299502cabe2f9f5753eb21a4fed468678
RahilZilani <37873621+RahilZilani@users.noreply.github.com>
2018-05-01 17:57:12 +0100

    update

--- DesignTechRibbon/Revit/MessageBoxForm/DTMessage.Designer.cs
+++ DesignTechRibbon/Revit/MessageBoxForm/DTMessage.Designer.cs
@@ -64,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Location = new System.Drawing.Point(6, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 25);
             this.label1.TabIndex = 6;

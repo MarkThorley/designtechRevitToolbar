2da439bc15712a1f33b7f4abfaeecdf21e92e628
RahilZilani <37873621+RahilZilani@users.noreply.github.com>
2018-05-10 15:20:04 +0100

    Commented and Removed Unused Code

--- DesignTechRibbon/Revit/MessageBoxForm/DTMessage.cs
+++ DesignTechRibbon/Revit/MessageBoxForm/DTMessage.cs
@@ -19,64 +19,33 @@ namespace DesignTechRibbon.Revit.MessageBoxForm
 
         public void ShowMessage(string MainText)
         {
-
-            //this.Size = new System.Drawing.Size(100, 100);
-            //this.Refresh();
-
-
-            label1.Text = MainText; //Gets the Input Text
-            label1.MaximumSize = new Size(groupBox1.Width-10, 0);
-            label1.AutoSize = true;
-
-            groupBox1.AutoSize = true;
-
-            if(groupBox1.Height > 200)
+            try
             {
-                this.Size = new System.Drawing.Size(this.Width, groupBox1.Height + (groupBox1.Height / 2));
+                label1.Text = MainText;                                     //Gets the Input Text
+                label1.MaximumSize = new Size(groupBox1.Width - 10, 0);     //Makes the label width almost the window size
+                label1.AutoSize = true;                                     //Label1 Will size based on the text
+                groupBox1.AutoSize = true;                                  //Group Box will Size based on the label
+
+                if (groupBox1.Height > 200)
+                {
+                    this.Size = new System.Drawing.Size(this.Width, groupBox1.Height + (groupBox1.Height / 2)); //Sets Width of the box to the form size and increases the height bu half
+                }
+                else
+                {
+                    this.Size = new System.Drawing.Size(this.Width, groupBox1.Height + 150); //Increases the height by a small amount
+                }
+
+                this.Refresh(); //Update Changes to the form 
+
+                this.ShowDialog();  //Display Form
+
+
             }
-            else
+            catch(Exception ex)
             {
-                this.Size = new System.Drawing.Size(this.Width, groupBox1.Height + 150);
+                MessageBox.Show("Error",ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-          
 
-            //this.Size = new System.Drawing.Size(1000, 1000);
-
-
-            // this.Size = new System.Drawing.Size(this.Width + (this.Width / 10), groupBox1.Height + (groupBox1.Height / 2));
-
-            //if (groupBox1.Height > this.Height)
-            //{
-            //   // groupBox1.Width = this.Width;
-            //   // groupBox1.Height = label1.Height + (label1.Height / 10);
-
-            //    this.Size = new System.Drawing.Size(this.Width + (this.Width / 10), groupBox1.Height + (groupBox1.Height / 2));
-
-            //}
-            //else
-            //{
-            //    this.Size = new System.Drawing.Size(500,500);
-            //}
-
-
-
-
-
-            this.Refresh();
-
-
-            //label1.AutoSize = true;
-
-            //if(label1.Height > groupBox1.Height)
-            //{
-            //    groupBox1.Height = label1.Height;
-            //    //this.Height = label1.Height;
-
-            //    this.Size = new System.Drawing.Size(groupBox1.Width,groupBox1.Height);
-            //    this.Refresh();
-            //}
-
-            this.ShowDialog();
 
         }
 
